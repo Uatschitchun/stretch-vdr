5b3f53f014603f79f0e039f2ec0977774194526b
Uatschitchun <anregend@yahoo.de>
2018-08-01 16:35:32 +0200

    Running!!

--- vdr-headless/vdr.sh
+++ vdr-headless/vdr.sh
@@ -9,14 +9,17 @@ export LANG="de_DE.UTF-8"
 
 # Set folder permissions
 # chown -r /recordings only if owned by root. We asume that means it's a docker volume
-[ "$(stat -c %u:%g /var/lib/video)" = "0:0" ] && chown -R vdr:vdr /var/lib/video/
-[ "$(stat -c %u:%g /var/lib/vdr/)" = "0:0" ] && chown -R vdr:vdr /var/lib/vdr/
-[ "$(stat -c %u:%g /var/cache/vdr)" = "0:0" ] && chown -R vdr:vdr /var/cache/vdr/
+#[ "$(stat -c %u:%g /var/lib/video)" = "0:0" ] && chown -R vdr:vdr /var/lib/video/
+#[ "$(stat -c %u:%g /var/lib/vdr/)" = "0:0" ] && chown -R vdr:vdr /var/lib/vdr/
+#[ "$(stat -c %u:%g /var/cache/vdr)" = "0:0" ] && chown -R vdr:vdr /var/cache/vdr/
+chown -R vdr:vdr /var/lib/video/
+chown -R vdr:vdr /var/lib/vdr/
+chown -R vdr:vdr /var/cache/vdr/
 
 # We need to preseed the volumes, just copy new files, no overwrite
 #cp -a -v --no-clobber /var/lib/vdr/* /vdr/config
 #cp -a /var/lib/vdr/plugins/epg2vdr/epg.dat /vdr/config/plugins/epg2vdr
 
 # Run vdr
-exec s6-setuidgid vdr vdr
+#exec s6-setuidgid vdr vdr
 
